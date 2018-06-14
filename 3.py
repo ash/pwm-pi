35d64bb9fb88ce9e3c41dd054f88268b2c08806d
Andrew Shitov <andy@shitov.ru>
2018-06-14 17:06:19 +0200

    Python example

---
+++ 3.py
@@ -0,0 +1,15 @@
+# An example from https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+
+import RPi.GPIO as GPIO
+
+j_pin = 12 # Note! This is the number of the phisical pin that correspond to GPIO 18
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(j_pin, GPIO.OUT)
+
+p = GPIO.PWM(j_pin, 18750) # Frequency in Hz
+p.start(30) # 30% PWM
+
+input('Press ENTER to stop')
+p.stop()
+GPIO.cleanup()

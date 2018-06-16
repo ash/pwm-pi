ad4638f80ad6839b0431ba5b9fe6c01dddc55abb
Andrew Shitov <andy@shitov.ru>
2018-06-16 15:31:26 +0000

    fix

--- hyst.cpp
+++ hyst.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // hyst == 1: turn hysteresis on
 // hyst == 0: turn hysteresis off
-const int hyst = 1;
+const int hyst = 0;
 
 const int pin = 21;
 
@@ -14,7 +14,6 @@ int main() {
     if (!bcm2835_init()) {
         return 1;
     }
-
 
     bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
     uint32_t* gpioBASE = bcm2835_regbase(BCM2835_REGBASE_GPIO);
@@ -35,8 +34,6 @@ int main() {
     bcm2835_peri_write(bcm2835_pads + BCM2835_PADS_GPIO_0_27 / 4, new_pads);
     pads = bcm2835_peri_read(bcm2835_pads + BCM2835_PADS_GPIO_0_27 / 4);
     cout << "New pads reg value:\t" << bitset<32>(pads) << endl;
-
-    bcm2835_peri_write(gpioBASE + BCM2835_GPSET0 / 4, 1);
 
     while(1) {
         int lev = bcm2835_gpio_lev(pin);
